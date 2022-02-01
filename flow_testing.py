2b141e627b94f6145b9d12d067b97e27a863d918
niels prins <niels.prins@gmail.com>
2022-02-01 21:43:47 +0100

    t

--- flow_testing.py
+++ flow_testing.py
@@ -1,3 +1,4 @@
+import os
 from flow.base_flow import compose_alter_flow, compose_flow
 
 # Just run in a local execution context
@@ -25,22 +26,23 @@ if 1 == 0:
     f = compose_alter_flow()
     f.storage = Local(
         stored_as_script=True,
-        path="/home/prinsn/Code/prefect/repo/pre-play/flow_testing.py"
+        path="/home/prinsn/Code/prefect/repo/pre-play/flow_testing.py",
     )
     f.executor = dask.LocalDaskExecutor(scheduler="processes")
     f.register(project_name="pre-play")
 
 
-# Dockerize the environment where the flow is running in 
+# Dockerize the environment where the flow is running in
 if 1 == 0:
     # project should be created, first time = prefect create project 'pre-play'
     from prefect.storage import Local
     from prefect.executors import dask
     from prefect.run_configs import DockerRun
+
     f = compose_alter_flow()
     f.storage = Local(
         stored_as_script=True,
-        path="/home/prinsn/Code/prefect/repo/pre-play/flow_testing.py"
+        path="/home/prinsn/Code/prefect/repo/pre-play/flow_testing.py",
     )
     f.executor = dask.LocalDaskExecutor(scheduler="processes")
     f.register(project_name="pre-play")
@@ -51,18 +53,34 @@ if 1 == 0:
     from prefect.storage import Docker
     from prefect.executors import dask
     from prefect.run_configs import DockerRun
+
     f = compose_alter_flow()
     # We inject the dockerfile because the import in the flow location path script requires the
     # flow package to be there, this is installed in the base dockerfile
     # Storage must be docker, can no longer be Local, docker agent does not accept this
     f.storage = Docker(
-        dockerfile="Dockerfile", 
+        dockerfile="Dockerfile",
         ignore_healthchecks=True,
         stored_as_script=True,
-        path="/APP/flow_testing.py"
+        path="/APP/flow_testing.py",
     )
-    
+
     f.run_config = DockerRun(image="pre-play:latest")
 
     f.executor = dask.LocalDaskExecutor(scheduler="processes")
+    f.register(project_name="pre-play")
+
+# Dockerize the environment where the flow is running in
+if 1 == 1:
+    # project should be created, first time = prefect create project 'pre-play'
+    from prefect.storage import GitHub
+    from prefect.executors import dask
+    from prefect.run_configs import DockerRun
+    f = compose_alter_flow()
+    f.storage = GitHub(
+        repo="prinsniels/pre-play",  # name of repo
+        path="flow_testing.py",  # location of flow file in repo
+        access_token_secret="MYSECRET"  # name of personal access token secret
+    )
+    # f.executor = dask.LocalDaskExecutor(scheduler="processes")
     f.register(project_name="pre-play")
